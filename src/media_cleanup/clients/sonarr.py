39e1ffb9e268e53a6593439d5da2ad775cf94d79
Spiritreader <earthstamper@gmail.com>
2024-08-14 18:25:10 +0200

    fix sonarr api

--- src/media_cleanup/clients/sonarr.py
+++ src/media_cleanup/clients/sonarr.py
@@ -1,6 +1,12 @@
+from typing import TypedDict
 import requests
 
-from media_cleanup.schema.sonarr_schema import Series, EpisodeFile
+from media_cleanup.schema.sonarr_schema import Season, Series, EpisodeFileResource
+
+class Result(TypedDict):
+    metadata: Series
+    original_path: str
+    Season: Season
 
 
 class SonarrClient:
@@ -9,34 +15,37 @@ class SonarrClient:
         self.api_key = api_key
 
     def get_file_path(self, file_paths: list[int]) -> list[str]:
-        res = requests.get(f'{self.root_url}/api/v3/series', headers=self._header)
+        res = requests.get(
+            f'{self.root_url}/api/v3/series', headers=self._header)
         res.raise_for_status()
         series_list = res.json()
 
-        matching_series: list[Series] = [series for series in series_list
-            if any(series["path"] in file_path for file_path in file_paths)]
-
-        # todo put
-        series_dict = {
-            series['id']: {
+        matching_series: list[dict] = [
+            {
                 "metadata": series,
-                "original_path": series["path"],
+                "original_path": file_path,
             }
+            for series in series_list
+            for file_path in file_paths
+            if series["path"] in file_path
+        ]
+
+        series_dict = {
+            series["metadata"]["id"]: series
             for series in matching_series
         }
 
-
         for series_id, series in series_dict.items():
-            res = requests.get(f"{self.root_url}/api/v3/episodefile?seriesId={series_id}", headers=self._header)
+            res = requests.get(
+                f"{self.root_url}/api/v3/episodefile?seriesId={series_id}", headers=self._header)
             res.raise_for_status()
-            episodes: list[EpisodeFile] = res.json()
-            for episode in episodes:
-                file_name = series["original_path"].replace(series["metadata"]["path"], "")
+            episodes: list[EpisodeFileResource] = res.json()
+            matched_episodes = [episode for episode in episodes if episode["path"] == series["original_path"]]
+            if matched_episodes:
+                matched_episode: EpisodeFileResource = matched_episodes[0]
+                series_dict[series_id]["Season"] = matched_episode["seasonNumber"]
 
-                pass
-
-
-        return matching_series
+        return series_dict
 
     @property
     def _header(self):
